5e404478df2975c53eaa1e77efd5a84a30b97dec
Charles Clinton Pustejovsky III <charles@cpustejovsky.com>
2025-06-17 18:30:37 -0400

    initial server code

---
+++ server/handlers/default.go
@@ -0,0 +1,46 @@
+package handlers
+
+import (
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+)
+
+var t = template.Must(template.New("hello").Parse("Hello, {{.}}!"))
+
+func DefaultHandler() http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			defer func(r io.ReadCloser) {
+				_, _ = io.Copy(io.Discard, r)
+				_ = r.Close()
+			}(r.Body)
+
+			var b []byte
+
+			switch r.Method {
+			case http.MethodGet:
+				b = []byte("friend")
+			case http.MethodPost:
+				var err error
+				b, err = io.ReadAll(r.Body)
+				if err != nil {
+					http.Error(w, "Internal server error",
+						http.StatusInternalServerError)
+					return
+				}
+			default:
+				// not RFC-compliant due to lack of "Allow" header
+				http.Error(w, "Method not allowed",
+					http.StatusMethodNotAllowed)
+				return
+			}
+
+			err := t.Execute(w, string(b))
+			if err != nil {
+				log.Println(err)
+			}
+		},
+	)
+}
